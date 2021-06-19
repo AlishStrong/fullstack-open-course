8b58cdc5cbd857b415538887e9acc6711670fbe7
Alisher <alisher.aliev@tieto.com>
2021-06-19 23:06:00 +0300

    part2: courseinfo 2.2 and 2.3

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -6,11 +6,14 @@ const Part = ({part}) => <p>{part.name} {part.exercises}</p>;
 
 const Content = ({parts}) => parts.map(part => <Part key={part.id} part={part} />);
 
+const Total = ({parts}) => <p><b>total of {parts.reduce((pr, cr) => pr + cr.exercises, 0)} exercises</b></p>
+
 const Course = ({course}) => {
   return (
     <>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   );
 }

7508d9bde7be5992414eb6581395f6fbd6c21f45
Alisher <alisher.aliev@tieto.com>
2021-06-19 09:58:35 +0300

    part1: unicafe 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [history, setHistory] = useState([]);
+
+  const giveFeedback = (rating) => () => {
+    switch (rating) {
+      case 1:
+        setGood(good + 1);
+        break;
+      case -1:
+        setBad(bad + 1);
+        break;
+      default:
+        setNeutral(neutral + 1)
+        break;
+    }
+    setHistory(history.concat(rating));
+  }
+
+  const getAverage = () => history.length > 0 ? history.reduce((pr, cr) => pr + cr, 0) / history.length : 0;
+
+  const getPositive = () => history.length > 0 ? good * 100 / history.length : 0;
 
   return (
     <div>
       <h1>give feedback</h1>
       <div>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <button onClick={giveFeedback(1)}>good</button>
+        <button onClick={giveFeedback(0)}>neutral</button>
+        <button onClick={giveFeedback(-1)}>bad</button>
       </div>
       <h1>statistics</h1>
       <div>
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {history.length}</p>
+        <p>average {getAverage()}</p>
+        <p>positive {getPositive()} %</p>
       </div>
     </div>
   )

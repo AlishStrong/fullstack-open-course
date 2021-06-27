f7a713d771bfbbf6b18bc693643b8868d3d7dcf1
Alisher <alisher.aliev@tieto.com>
2021-06-27 21:55:26 +0300

    part5: blog list app frontend 5.15

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -55,3 +55,40 @@ describe('5.14: Blog list tests, step2', () => {
     expect(component.container).toHaveTextContent('0');
   });
 });
+
+describe('5.15*: Blog list tests, step3', () => {
+  test('<Blog /> button clicked twice', () => {
+    const blog = {
+      author: 'Author3',
+      id: '60d87145ebe55c1150eaf10c',
+      likes: 0,
+      title: 'Blog3',
+      url: 'URL3',
+      user: {
+        username: 'new_user',
+        name: 'New User',
+        id: '60d70f2837757d36bc3a3f77'
+      }
+    };
+
+    const mockHandler = jest.fn();
+
+    const component = render(
+      <Blog blog={blog} likeBlog={mockHandler} removeBlog={() => {}} username={'null'} />
+    );
+
+    const button = component.container.querySelector('.showDetails');
+    fireEvent.click(button);
+
+    expect(button).toHaveTextContent('hide');
+    expect(component.container).toHaveTextContent('URL3');
+    expect(component.container).toHaveTextContent('0');
+
+    const likeButton = component.container.querySelector('.likeButton');
+    expect(likeButton).toBeDefined();
+    fireEvent.click(likeButton);
+    fireEvent.click(likeButton);
+
+    expect(mockHandler.mock.calls).toHaveLength(2);
+  });
+});
